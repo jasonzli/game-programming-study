943aa2ff02d075bd5c541be82474fca7c52d0aaf
Jason Li <jasonli1337@gmail.com>
2020-06-08 15:56:10 -0700

    Added Basic Movement and Abstract Command

---
+++ Programming Patterns/Assets/Scripts/Command Class.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command
+{
+    public abstract void Execute();
+    public virtual void Undo() { }
+}
+
+
+//Ab
+public abstract class MoveCommand : Command
+{
+    protected float moveDistance = 1f;
+
+
+}
+
+public class MoveForward : MoveCommand
+{
+    public override void Execute(){
+
+    }
+
+    public override void Undo(){
+
+    }
+}
+/*
+public class MoveCommand : Command
+{
+    public override void Execute(Transform targetTransform, Command command)
+    {
+        Move(targetTransform);
+    }
+
+    public override void Undo(Transform targetTransform, Command command)
+    {
+        UnMove(targetTransform);
+    }
+
+    public void Move(Transform targetTransform){
+
+    }
+    public  void UnMove(Transform targetTransform){
+
+    }
+
+}*/
---
+++ Programming Patterns/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField]
+    private float MoveDistance = 1f;
+    private Transform boxTransform;
+
+    #region Buttons
+    
+    #endregion
+
+    void Start(){
+        boxTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ReadInput();
+    }
+
+    void ReadInput(){
+        
+        if (Input.GetKeyDown(KeyCode.W)){
+            boxTransform.Translate(boxTransform.up*MoveDistance);
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
+            boxTransform.Translate(-boxTransform.right*MoveDistance);
+        }
+        if (Input.GetKeyDown(KeyCode.S)){
+            boxTransform.Translate(-boxTransform.up*MoveDistance);
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            boxTransform.Translate(boxTransform.right*MoveDistance);
+        }
+        
+    }
+}

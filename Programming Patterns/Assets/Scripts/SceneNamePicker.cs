8e8bc6535186decfbd368009e15ba44dfc926803
Jason Li <jasonli1337@gmail.com>
2020-07-06 10:31:59 -0700

    Reflection Probes

---
+++ Programming Patterns/Assets/Scripts/SceneNamePicker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[AttributeUsage(AttributeTargets.Field)]
+public class SceneNamePicker : PropertyAttribute
+{
+    public bool showPath = true;
+
+    public SceneNamePicker() {}
+    public SceneNamePicker(bool showPath){
+        this.showPath = showPath;
+    }
+}
--- Programming Patterns/Assets/Scripts/SceneSelect.cs
+++ Programming Patterns/Assets/Scripts/SceneSelect.cs
@@ -6,12 +6,22 @@ using UnityEngine.SceneManagement;
 public class SceneSelect : MonoBehaviour
 {
 
+    [SceneNamePicker]
+    public string SceneName;
+
+    [SceneNamePicker(showPath = false)]
+    public string OtherSceneName;
+    
     public void GoToCommandScene(){
         GoToScene("Command Scene");
     }
 
     public void GoToFlyWeightScene(){
         GoToScene("Flyweight Scene");
+    }
+
+    public void GoToPickScene(){
+        GoToScene("Pick Scene");
     }
     private void GoToScene(string sceneName){
         SceneManager.LoadScene(sceneName);

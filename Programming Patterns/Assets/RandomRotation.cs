82c4ab7f4ba15fcf42315125b285857987b89d8e
Jason Li <jasonli1337@gmail.com>
2020-07-12 17:25:31 -0700

    Picker Scene UPdated
    New scene selection so that you can move between the scenes better

---
+++ Programming Patterns/Assets/RandomRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    public Vector3 rotationDirection;
+    public float rotationSpeed = 52f;
+
+    void OnEnable(){
+        rotationDirection = new Vector3(
+            Random.Range(-1f,1f),
+            Random.Range(-1f,1f),
+            Random.Range(-1f,1f)
+        );
+
+        rotationDirection = rotationDirection.normalized;
+    }
+    void Update()
+    {
+        transform.Rotate(Time.deltaTime * rotationSpeed * rotationDirection,Space.Self);
+    }
+}

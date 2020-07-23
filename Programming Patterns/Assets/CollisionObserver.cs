79dffb3825469affb5cfb4cbbd2a17420ced5004
Jason Li <jasonli1337@gmail.com>
2020-07-22 21:50:22 -0400

    Basic Watcher Events

---
+++ Programming Patterns/Assets/CollisionObserver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CollisionObserver : MonoBehaviour
+{
+    public Action<Vector3> PlaneIntersected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        PlaneIntersected?.Invoke(other.transform.position);
+
+    }
+}

7e5b5305f4473b11c33a5147ada8a04a65ec2467
Jason Li <jasonli1337@gmail.com>
2020-09-08 00:07:46 -0400

    Enum State
    Implemented a basic enum state.
    
    Easy enough actually!

---
+++ Programming Patterns/Assets/CubeClassState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeClassState : MonoBehaviour
+{
+    private CubeState _state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _state = new IdleState();
+        _state.Enter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _state.Update();
+    }
+}
+
+public class State
+{
+    public virtual void Enter() {}
+    public virtual void Exit() {}
+}
---
+++ Programming Patterns/Assets/CubeHero.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum EnumState{
+    STATE_IDLE,
+    STATE_JUMPING
+}
+
+public class CubeHero : MonoBehaviour
+{
+
+    [SerializeField]
+    private EnumState CubeState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CubeState = EnumState.STATE_IDLE;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+
+    void HandleInput(){
+        switch(CubeState)
+        {
+            case (EnumState.STATE_IDLE):
+                if (Input.GetKeyDown("space")){
+                    GetComponent<Rigidbody>().AddForce(transform.up * 300);
+                    CubeState = EnumState.STATE_JUMPING;
+                }
+                break;
+            case (EnumState.STATE_JUMPING):
+
+                break;
+        }
+    }
+
+    void OnCollisionEnter(Collision contact){
+        if (contact.gameObject.CompareTag("Floor"))
+            CubeState = EnumState.STATE_IDLE;
+    }
+}

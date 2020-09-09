ab3184cf1dc1ffaefaaaad07b3342d99e74e1301
Jason Li <jasonli1337@gmail.com>
2020-09-09 02:37:00 -0400

    Something is wrong with how I'm thinking about this problem
    Where should dependency lie? How does state get to that dependency?

--- Programming Patterns/Assets/Scripts/State/Cube.cs
+++ Programming Patterns/Assets/Scripts/State/Cube.cs
@@ -7,9 +7,19 @@ public class Cube : MonoBehaviour{
 
     FiniteStateMachine<Cube> FSM;
 
+
+    [SerializeField] float maxJumpHeight = 10f;
+    public float MaxJumpHeight {get; private set;}
+    [SerializeField] float upwardSpeed = 10f;
+    public float UpwardSpeed {get; private set;}
+
     private void Start() {
         FSM = new FiniteStateMachine<Cube>(this);
         FSM.ChangePendingState(new Idle<Cube>(this));
+    }
+    
+    public FiniteStateMachine<Cube> GetFSM(){
+        return FSM;
     }
 
     private void Update(){
@@ -17,10 +27,10 @@ public class Cube : MonoBehaviour{
     }
 }
 
-public class Idle<TContext> : State<TContext> where TContext : MonoBehaviour{
+public class Idle<Cube> : State<Cube> where Cube : MonoBehaviour{
     
-    private TContext Parent;
-    public Idle (TContext _Parent){
+    private Cube Parent;
+    public Idle (Cube _Parent){
         Parent = _Parent;
     }
 
@@ -31,12 +41,37 @@ public class Idle<TContext> : State<TContext> where TContext : MonoBehaviour{
         HandleInput(Parent);
     }
 
-    void HandleInput(TContext context){
+    void HandleInput(Cube context){
+        
+        Debug.Log($"Machine has entered Idle state");
+        if (Input.GetKeyDown("space")){
+            Parent.GetFSM().ChangeState(new Jumping<Cube>(Parent));
+            Parent.transform.GetComponent<Rigidbody>().AddForce(Parent.transform.up * 250);
+        }
+    }
+}
+//yeah the stuff after Jump<TContext> seems too redundant.
+public class Jumping<Cube> : State<Cube> where Cube : MonoBehaviour{
+    
+    private Cube Parent;
+    
+    public Jumping (Cube _Parent){
+        Parent = _Parent;
+    }
+
+    public override void Enter(){
+    }
+    public override void Update(){
+        Parent.transform.position = Parent.transform.up * Parent.UpwardSpeed;
+        //HandleInput(Parent);
+    }
+
+    void HandleInput(Cube context){
         
         Debug.Log($"Machine has entered Idle state");
         if (Input.GetKeyDown("space")){
             //Parent.ChangeState(new Jumping<TContext>());
-            Parent.transform.GetComponent<Rigidbody>().AddForce(Parent.transform.up * 250);
+            //Parent.transform.GetComponent<Rigidbody>().AddForce(Parent.transform.up * 250);
         }
     }
 }

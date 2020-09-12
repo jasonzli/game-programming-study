9e22cb9a203be3bc98c03ea1501a1a94ce541886
Jason Li <jasonli1337@gmail.com>
2020-09-11 23:16:30 -0400

    New State machine approach

---
+++ Programming Patterns/Assets/Scripts/State/StatePattern.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Patterns
+{
+    public class State{
+
+    }
+
+    public class FSM{
+        
+    }
+}
